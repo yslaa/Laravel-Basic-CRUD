2732dbf91866d2c3c0c30d9279276b93a1a2921b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-01-19 16:34:15 +0800

    Add investor route

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,11 @@ Route::get('/', function () {
 
 Route::get('test', 'testController@test');
 Route::resource("/customer", "customerController");
+Route::resource("/investor", "investorController");
 
 Route::get('/personnel', 'personnelController@index');
-Route::get('/personnel/create', 'personnelController@create')->name('personnels.create');
-Route::post('/personnel/store', ['uses' => 'personnelController@store', 'as' => 'personnels.store']);
+Route::get('/personnel/create', 'personnelController@create')->name('personnels.create'); //option 1 best for me
+Route::post('/personnel/store', ['uses' => 'personnelController@store', 'as' => 'personnels.store']); //option 2
 Route::get('/personnel/edit/{id}', 'personnelController@edit')->name('personnels.edit');
 Route::post('/personnel/update{id}', ['uses' => 'personnelController@update', 'as' => 'personnels.update']);
 Route::delete('/personnel/destroy/{id}', ['uses' => 'personnelController@destroy', 'as' => 'personnels.destroy']);
