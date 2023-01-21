456bf397fa2382964c2f054ae180341a67a0fe01
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-01-21 23:06:18 +0800

    Add tailwind with vite 3

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -4,9 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/js/app.js')
 </head>
 
-<body>
+<body
+    style="background-image:linear-gradient(rgba(212, 212, 212, 0.1),rgba(212,212,212,0.1)), url(https://wallpapercave.com/wp/B1sODrM.jpg); background-size:cover; overflow-x: hidden;">
     @yield('contents')
 </body>
 
